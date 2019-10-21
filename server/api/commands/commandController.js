a096214bb4a2672bba896fde736ed36f27a17965
Tomas Maiorino <tomasmaiorino@gmail.com>
2019-10-22 00:00:11 +0100

    change log

--- server/api/commands/commandController.js
+++ server/api/commands/commandController.js
@@ -1,22 +1,22 @@
 const Command = require('./command');
 
 async function findById(commandId) {
-  console.debug('controller -> Looking for the command  ' + commandId);
+  //console.debug('controller -> Looking for the command  ' + commandId);
   return Command.findById(commandId);
 }
 
 async function findByTag(tagValue) {
-  console.debug('looking command by tag %j.', tagValue);
+  //console.debug('looking command by tag %j.', tagValue);
   return Command.find({ "tags": { $regex: tagValue, $options: 'i' } });
 }
 
 async function save(command) {
-  console.debug('creating command');
+  //console.debug('creating command');
   return Command.create(command);
 }
 
 async function search(query) {
-  console.debug('looking for ' + query);
+  //console.debug('looking for ' + query);
   return Command.find({
     $or: [
       { "full_description": { $regex: query, $options: 'i' } },

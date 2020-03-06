15dd5ac20781949e480db23edd2fcd67582e8d74
Tomas Maiorino <tomasmaiorino@gmail.com>
2020-03-06 23:38:19 +0000

    remove invalid require

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const firebaseAdmin = require('firebase-admin')
 const app = express();
 const cors = require('cors');
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount)

50d95bfdb6730801910075762342807891b90946
Tomas Maiorino <tomasmaiorino@gmail.com>
2020-03-09 22:26:45 +0000

    testing heroku redirect

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path');
 
 //admin rounts
 const adminProjects = require('./api/career/projects/admin/projectRoute');

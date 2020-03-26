1da6c39928a9f7912b08c2029f0d7d0d34d4fadf
Tomas Maiorino <tomasmaiorino@gmail.com>
2020-03-26 00:30:47 +0000

    fix config environment

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,16 +1,16 @@
 'use strict';
 
 // config.js
-const env = process.env.NODE_ENV || 'dev'// 'dev' or 'test'
+const env = process.env.NODE_ENV;// 'dev' or 'test'
 
 let envValues = {
     server: {
-        url: 'http://127.0.0.1:8080'
+        url: ''
     }
 };
 
-if (env === 'prod') {
-    envValues.server.url = '';
+if (env === 'dev') {
+    envValues.server.url = 'http://127.0.0.1:8080';
 }
 
 module.exports = envValues;
